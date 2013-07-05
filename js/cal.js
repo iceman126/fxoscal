5f7e62e6e7493fa1c8ae32b444d5e357c621b72d
iceman126 <iceman1216i@gmail.com>
2013-07-05 15:30:53 +0800

    fundamental structure

---
+++ js/cal.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var cal = {
+	calculateResult: function calculateResult(e) {
+		var first = parseFloat(cal_form.first.value);
+		var second = parseFloat(cal_form.second.value);
+	}
+}
