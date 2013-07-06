97ef3c319c8010433ff066e9c3249dee4ef27b42
iceman126 <iceman1216i@gmail.com>
2013-07-06 22:19:21 +0800

    fundamental functions are completed
    try make it beautiful (css) if have time

--- js/cal.js
+++ js/cal.js
@@ -1,8 +1,48 @@
 'use strict';
 
-var cal = {
-	calculateResult: function calculateResult(e) {
-		var first = parseFloat(cal_form.first.value);
-		var second = parseFloat(cal_form.second.value);
-	}
-}
+function plus() {
+	//e.preventDefault();
+	var first = parseFloat(document.getElementById('first').value);
+	var second = parseFloat(document.getElementById('second').value);
+	var result = first + second;
+	//alert(result);
+	document.getElementById("calc_result").innerHTML = result;
+};
+function minus() {
+	//e.preventDefault();
+	var first = parseFloat(document.getElementById('first').value);
+	var second = parseFloat(document.getElementById('second').value);
+	var result = first - second;
+	//alert(result);
+	document.getElementById("calc_result").innerHTML = result;
+};
+function multi() {
+	//e.preventDefault();
+	var first = parseFloat(document.getElementById('first').value);
+	var second = parseFloat(document.getElementById('second').value);
+	var result = first * second;
+	//alert(result);
+	document.getElementById("calc_result").innerHTML = result;
+};
+function divide() {
+	//e.preventDefault();
+	var first = parseFloat(document.getElementById('first').value);
+	var second = parseFloat(document.getElementById('second').value);
+	var result = first / second;
+	//alert(result);
+	document.getElementById("calc_result").innerHTML = result;
+};
+
+
+function init() {
+	document.getElementById('plus').addEventListener('click', plus, false);
+	document.getElementById('minus').addEventListener('click', minus, false);
+	document.getElementById('multi').addEventListener('click', multi, false);
+	document.getElementById('divide').addEventListener('click', divide, false);
+};
+
+
+window.addEventListener('load', function browserOnLoad(evt) {
+	window.removeEventListener('load', browserOnLoad);
+	init();
+});
